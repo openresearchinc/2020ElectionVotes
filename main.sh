4e0b80d2c32b9e7550c26b8337c9ed398788dc61
Qiming He <qiming.he@openresearchinc.com>
2020-11-12 06:58:16 +0000

    finish 50 states, and compute vote diff vs. time

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
-#ref: https://m.wenxuecity.com/bbs/rdzn/4647564.html
+#!/bin/bash
 
-for state in pennsylvania minnesota arizona nevada north-carolina 
-do #battle state first and then 
+for state in alabama alaska arizona arkansas california colorado delaware florida georgia hawaii idaho illinois indiana iowa kansas kentucky  \
+  louisiana maine maryland massachusetts michigan minnesota mississippi missouri montana nebraska nevada new-hampshire new-jersey new-mexico  \
+  new-york north-carolina north-dakota ohio oklahoma oregon pennsylvania rhode-island south-carolina south-dakota tennessee texas utah vermont\
+  virginia washington west-virginia wisconsin wyoming district-of-columbia
+do  
   curl "https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/$state/president.json" |jq '.'> $state.json
 done
--- parse.sh
+++ parse.sh
@@ -1,17 +1,15 @@
-for state in pennsylvania minnesota arizona nevada north-carolina
-do
-  >trump.$state
-  >biden.$state
-  for i in $(jq '.data.races[0].timeseries| keys | .[] ' $state.json); 
-    do
-      votes=$(jq -r ".data.races[0].timeseries[$i] .votes" $state.json) #votes number in the time slot
-      trump=$(jq -r ".data.races[0].timeseries[$i] .vote_shares.trumpd" $state.json) #% of votes for Trump
-      biden=$(jq -r ".data.races[0].timeseries[$i] .vote_shares.bidenj" $state.json) #% of votes for Biden
-      #Votes number for each, should be strictly increasing (non-decreasing) function of time      
-      echo "$votes $trump" |awk '{print int ($1 * $2)}' >> trump.$state 
-      echo "$votes $biden" |awk '{print int ($1 * $2)}' >> biden.$state      
-    done
-    #Any descreasing in votes (#vote_t1 - #vote_t0)
-    cat trump.$state | awk 'p{print $0-p}{p=$0}' > trump-delta.$state
-    cat biden.$state | awk 'p{print $0-p}{p=$0}' > biden-delta.$state
+#!/bin/bash
+
+for state in alabama alaska arizona arkansas california colorado delaware florida georgia hawaii idaho illinois indiana iowa kansas kentucky 	\
+	louisiana maine maryland massachusetts michigan minnesota mississippi missouri montana nebraska nevada new-hampshire new-jersey new-mexico  \
+	new-york north-carolina north-dakota ohio oklahoma oregon pennsylvania rhode-island south-carolina south-dakota tennessee texas utah vermont\
+	virginia washington west-virginia wisconsin wyoming district-of-columbia
+do  
+	jq -r ".data.races[].timeseries[]|.timestamp,.votes,.vote_shares.trumpd" $state.json |tr '\n' ',' |sed 's/2020-11/\n2020-11/g' \
+		|tail -n +2 |awk -F',' '{printf ("%s,%0.f\n",$1, $2 * $3)}' > trump.$state   #timestamp,trump-votes
+	jq -r ".data.races[].timeseries[]|.timestamp,.votes,.vote_shares.bidenj" $state.json |tr '\n' ',' |sed 's/2020-11/\n2020-11/g' \
+		|tail -n +2 |awk -F',' '{printf ("%s,%0.f\n",$1, $2 * $3)}' > biden.$state   #timestamp,biden-votes
+		
+	cat trump.$state |tail -n +5 |awk -F',' 'p{print $1","$2-p}{p=$2}' > trump-delta.$state #timestamp, #Trump vote(t_n-t_(n-1))
+	cat biden.$state |tail -n +5 |awk -F',' 'p{print $1","$2-p}{p=$2}' > biden-delta.$state #timestamp, #Biden vote(t_n-t_(n-1))
 done
