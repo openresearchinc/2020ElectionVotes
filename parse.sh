83b5f1312459e1e6fd6d791421d09ae5dae8cf77
Qiming He <qiming.he@openresearchinc.com>
2020-11-13 23:02:55 +0000

    put trump/biden in one bar chart per state for gnuplot

--- parse.sh
+++ parse.sh
@@ -10,6 +10,7 @@ do
 	jq -r ".data.races[].timeseries[]|.timestamp,.votes,.vote_shares.bidenj" $state.json |tr '\n' ',' |sed 's/2020-11/\n2020-11/g' \
 		|tail -n +2 |awk -F',' '{printf ("%s,%0.f\n",$1, $2 * $3)}' > biden.$state   #timestamp,biden-votes
 		
-	cat trump.$state |tail -n +5 |awk -F',' 'p{print $1","$2-p}{p=$2}' > trump-delta.$state #timestamp, #Trump vote(t_n-t_(n-1))
-	cat biden.$state |tail -n +5 |awk -F',' 'p{print $1","$2-p}{p=$2}' > biden-delta.$state #timestamp, #Biden vote(t_n-t_(n-1))
+	cat trump.$state |awk -F',' 'p{print $1","$2-p}{p=$2}' | awk -F',' '{if ($2>0) print $1" 0";else print $1" "$2}' > trump-delta.$state #timestamp, #Trump drop #vote adj timeslot
+	cat biden.$state |awk -F',' 'p{print $1","$2-p}{p=$2}' | awk -F',' '{if ($2>0) print $1" 0";else print $1" "$2}' > biden-delta.$state #timestamp, #Biden drop #vote adj timeslot
+  join -j 1 -o 1.1 1.2 2.2 trump-delta.$state biden-delta.$state > $state.data #timestamp trump-lost biden-lost
 done
